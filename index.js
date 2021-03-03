c796b0a0ce4b325f456d293b022a55f964b065d2
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-03 09:59:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const fast = require("fs")
+
+const app = express()
+
+app.use(express.json())
+
+let data = JSON.parse(fs.readFileSync("./data.json"))
+
+
+app.post("/api/input", function(req, res) {
+    const json = req.body
+    console.log(req.body)
+    data.concat(json)
+    fast.writeFileSync("./data.json", JSON.stringify(data))
+    res.send("yes")
+})
+
+app.get("/api/input", (req, res) => res.json(data))
+app.use(express.static('public'))
+
+app.listen(process.env.PORT || 5000)
