85911e05064ef9e94f9eb80c05e5dd31095bd8c5
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-03 17:53:40 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -25,7 +25,7 @@ function load(arrayOfData) {
     dataHtml.innerHTML += tableHtml
     const mapd = L.map('mapid').setView([50.673 , 4.6], 13);
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(mapd)
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {}).addTo(mapd);
     L.polyline(arrayOfData.map(obj => [obj.lat, obj.long])).addTo(mapd)
 
     L.marker([51.5, -0.09]).addTo(mapd)
