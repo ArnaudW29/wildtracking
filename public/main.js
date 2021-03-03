d73acc66ca723c95b3b44fb8b87616e20aa9ff05
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-03 17:35:13 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -4,7 +4,26 @@ document.addEventListener("DOMContentLoaded", init)
 function init() {
     fetch("/api/input")
         .then(res => res.json())
+        .then(showData)
+
 }
+function showData(arrayOfData) {
+    const dataHtml = document.querySelector("#data table tbody")
+
+    // TODO: make a table of data
+    
+    let tableHtml = ""
+    for (const obj of arrayOfData)
+        tableHtml += `
+        <tr>
+            <th>${obj.lat}</th>
+            <th>${obj.long}</th>
+            <th>${obj.alt}</th>
+        </tr>
+        `
+
+    dataHtml.innerHTML += tableHtml
+
 const mapd = L.map('mapid').setView([50.673 , 4.6], 13);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(mapd)
