46f21f0109d147adcae2e1c9ab85e2ba52c9d970
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-03 17:32:50 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -7,8 +7,5 @@ function init() {
 }
 const mapd = L.map('mapid').setView([50.673 , 4.6], 13);
 
-	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-	tileSize: 512,
-	zoomOffset: -1
-	}).addTo(mapd);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(mapd)
 L.marker([51.5, -0.09]).addTo(mapd);
