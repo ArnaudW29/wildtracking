eecf9ca602071d96527811dbdf5813fee4bb4127
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-03 18:21:22 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -11,7 +11,7 @@ function init() {
 function showData(arrayOfData) {
     for (const obj of arrayOfData)
     var mapd = L.map('mapid').setView([50.48, 4.6], 10);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(mapd)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(mapd)
     L.polyline(arrayOfData.map(obj => [obj.lat, obj.long])).addTo(mapd)
   
     var obj = arrayOfData[arrayOfData.length - 1]
