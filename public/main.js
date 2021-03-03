40cf33015c1511302aca3e5e87517040fefc44f7
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-03 17:36:44 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -26,5 +26,5 @@ function showData(arrayOfData) {
 
 const mapd = L.map('mapid').setView([50.673 , 4.6], 13);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(mapd)
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(mapd);
 L.marker([51.5, -0.09]).addTo(mapd);
