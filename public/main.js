3891b2a2e5b95086a9feb828445c553027d8cf97
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-03 10:38:57 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,9 +1,11 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", init)
-
+function init() {
+    fetch("/api/input")
+        .then(res => res.json())
 const mymap = L.map('mapid').setView([50.673 , 4.6], 13);
-
+}
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	tileSize: 512,
 	zoomOffset: -1
