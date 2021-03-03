f9691f25c0fd78924f414c0f5880e61359a4410c
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-03 10:34:28 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,4 +1,8 @@
-var mymap = L.map('mapid').setView([50.673 , 4.6], 13);
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init)
+
+const mymap = L.map('mapid').setView([50.673 , 4.6], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	tileSize: 512,
