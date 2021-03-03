26548e965a91dea2db2e0dda541f12d09816c0b2
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-03 10:01:22 +0100

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,7 @@
+var mymap = L.map('mapid').setView([50.673 , 4.6], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+	tileSize: 512,
+	zoomOffset: -1
+}).addTo(mymap);
+L.marker([51.5, -0.09]).addTo(mymap);
