7b335303d7e71330f787d8d57858eb00e3c2b2dc
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-03 18:27:17 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -11,7 +11,6 @@ function init() {
 function showData(arrayOfData) {
     var mapd = L.map('mapid').setView([50.48, 4.6], 10);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(mapd)
-    L.polyline(arrayOfData.map(obj => [obj.lat, obj.long])).addTo(mapd)
   
     var obj = arrayOfData[arrayOfData.length - 1]
     L.marker([obj.lat, obj.long]).addTo(mymap)
