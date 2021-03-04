82679e7ca1a89ee073e65dcbe40430d79b930946
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-04 19:58:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ let data = JSON.parse(fs.readFileSync("./data.json"))
 
 app.post("/api/input", function(req, res) {
     const json = req.body
-    res.json(req.body)
-    console.log("resjson " + res.json(req.body))
     console.log(json)
     data.concat(json)
     fs.writeFileSync("./data.json", JSON.stringify(data))
