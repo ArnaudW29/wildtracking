17e43b5654a86855cbf3d8870e230e205fb16472
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-05 00:17:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post("/api/input", function(req, res) {
     const json = req.body
     console.log(json)
     data.concat(json)
-    fs.writeFileSync("./data.json", JSON.stringify(json))
+    fs.writeFileSync("main.js", JSON.stringify(json))
     res.send("Received")
 })
 
