b0c98f368b806601bdd9565e216673663c1cda8d
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-04 23:36:39 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -200,6 +200,47 @@ function draw_map() {
 }
 
 
+function init() {
+    fetch("/api/input")
+        .then(res => res.json())
+        // .then(() => [
+        //     {
+        //         lat: 50.48,
+        //         long: 4.6,
+        //         alt: 50.48
+        //     }, {
+        //         lat: 50.79878,
+        //         long: 4.8,
+        //         alt: 50.48
+        //     }, {
+        //         lat: 50.498,
+        //         long: 4.8098,
+        //         alt: 50.48
+        //     }
+        // ])
+        .then(showData)
+}
+function showData(arrayOfData) {
+    const dataHtml = document.querySelector("#data table tbody")
+
+    // TODO: make a table of data
+    
+    let tableHtml = ""
+    for (const obj of arrayOfData)
+        tableHtml += `
+        <tr>
+            <th>${obj.lat}</th>
+            <th>${obj.long}</th>
+            <th>${obj.alt}</th>
+        </tr>
+        `
+
+    dataHtml.innerHTML += tableHtml
+
+    // SHOW MAP
+    const obj = arrayOfData[arrayOfData.length - 1]
+    L.marker([obj.lat, obj.long]).addTo(mymap)
+}
 
 
 function addDatas(données){
