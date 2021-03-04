6f79a378c27435496abdda65f1a12ec417a92d15
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-04 11:10:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ const app = express()
 
 app.use(express.json())
 
-let data = JSON.parse(fs.readFileSync("./data.json"))
 
 
 app.post("/api/input", function(req, res) {
     const json = req.body
     console.log(req.body)
     data.concat(json)
-    fs.writeFileSync("./data.json", JSON.stringify(data))
     console.log("POST request")
-    console.log(data)
-    console.log(fs.writeFileSync("./data.json", JSON.stringify(data)))
 });
 
 app.get("/api/input", (req, res) => res.json(data))
