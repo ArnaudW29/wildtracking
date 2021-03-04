57d7676793b93e602164d3d4db6d7f24f042ad36
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2021-03-04 11:17:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ app.use(express.json())
 app.post("/api/input", function(req, res) {
     const json = req.body
     console.log(req.body)
+    fs.writeFileSync("./data.json", JSON.stringify(data))
     data.concat(json)
     console.log("POST request")
 });
 
-app.get("/api/input", (req, res))
+app.get("/api/input", (req, res) => res.json(data))
 app.use(express.static('public'))
 app.listen(process.env.PORT || 5000,function() {
     console.log("server is running");
